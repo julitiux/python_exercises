b710cd154598db42b6e2ad7be170508962971f3f
Julio Ramirez <rrodriguez.julio@gmail.com>
2020-12-17 19:23:53 -0600

    change the name of folder

--- Dictionaries/favorite_languages.py
+++
@@ -1,8 +0,0 @@
-favorite_languages={
-    'jen':'python',
-    'sarah':'c',
-    'edward':'tuby',
-    'phil':'python'
-    }
-
-print("Sara's favorite language is " + favorite_languages['sarah'].title())
--- Dictionaries/user.py
+++
@@ -1,9 +0,0 @@
-user = {
-    'suername':'efermi',
-    'first':'enrico',
-    'last':'fermi'
-    }
-
-for key, value in user.items():
-    print("\nKey:" + key)
-    print("Value:" + value)

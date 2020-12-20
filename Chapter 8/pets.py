4e9dd143e577eb6d66cf1c01c665ebac7a2e32d4
Julio Ramirez <rrodriguez.julio@gmail.com>
2020-12-19 23:19:52 -0600

    add keyword arguments

--- Chapter 8/pets.py
+++ Chapter 8/pets.py
@@ -3,5 +3,5 @@ def describe_pet(animal_type, pet_name):
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
-describe_pet('hmster', 'harry')
-describe_pet('dog', 'willie')
+describe_pet(animal_type='hmster', pet_name='harry')
+describe_pet(animal_type='dog', pet_name='willie')

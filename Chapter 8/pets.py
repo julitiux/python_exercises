e1a30d8617cfea34d94f1200f97d82a030e9c403
Julio Ramirez <rrodriguez.julio@gmail.com>
2020-12-19 23:24:30 -0600

    add default values

--- Chapter 8/pets.py
+++ Chapter 8/pets.py
@@ -1,7 +1,9 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name,animal_type='dog'):
     """Display information about a pet."""
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
 describe_pet(animal_type='hmster', pet_name='harry')
 describe_pet(animal_type='dog', pet_name='willie')
+
+describe_pet(pet_name='Baby Drogon')

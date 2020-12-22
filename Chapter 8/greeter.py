cc728a4e6fc19c22adedec1f8244f19b1de9373b
Julio Ramirez <rrodriguez.julio@gmail.com>
2020-12-22 00:53:21 -0600

    add greeter.py

--- Chapter 8/greeter.py
+++ Chapter 8/greeter.py
@@ -1,6 +1,12 @@
-def greet_user():
-    """Display a simple greeeting."""
-    print("Hello animal!")
+def get_formatter_name(first_name, last_name):
+    """Return a full_name , neatly formatted"""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
 
+while True:
+    print("\nPlease tell me your name:")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
 
-greet_user()
+    formatted_name = get_formatter_name(f_name, l_name)
+    print("\nHello, " + formatted_name + "!")

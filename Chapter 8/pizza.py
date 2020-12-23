7e7d7d2c5c56a0d20b02d922968a3ceb32e21fa5
Julio Ramirez <rrodriguez.julio@gmail.com>
2020-12-22 23:15:44 -0600

    add size of pisnhy pizza

--- Chapter 8/pizza.py
+++ Chapter 8/pizza.py
@@ -1,8 +1,8 @@
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """Print the list of toppings that hace been request"""
-    print("\nMaking a pizza with the following toppings: ")
+    print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
     for topping in toppings:
         print("- " + topping)
 
-make_pizza('pepperoni')
-make_pizza('mushrooms','green peppers','extra cheese')
+#make_pizza('pepperoni')
+#make_pizza('mushrooms','green peppers','extra cheese')

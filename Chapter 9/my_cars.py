9118477f32f0482d7cde791727689e14a5d59d89
Julio Ramirez <rrodriguez.julio@gmail.com>
2020-12-29 18:48:37 -0600

    use import keyword

--- Chapter 9/my_cars.py
+++ Chapter 9/my_cars.py
@@ -1,7 +1,7 @@
-from electric_car import Car, ElectricCar
+import electric_car
 
-my_beetle = Car('volkswagen','beetle',2016)
+my_beetle = electric_car.Car('volkswagen','beetle',2016)
 print(my_beetle.get_descriptive_name())
 
-my_tesla = ElectricCar('tesla','roadster',2016)
+my_tesla = electric_car.ElectricCar('tesla','roadster',2016)
 print(my_tesla.get_descriptive_name())

dd62b94dcaaaeb8b4941f52f67f97fb850f35348
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-07 23:45:03 -0600

    add part of chapter 10

---
+++ Chapter 10/pi_string.py
@@ -0,0 +1,21 @@
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+
+print(pi_string[:52] + "...")
+
+print(pi_string)
+print(len(pi_string))
+
+
+birthday = input("Enter your bithday, in the form mmddyy: ")
+
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi")

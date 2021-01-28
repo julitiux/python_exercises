8e8f692f06cc9bba9a26b734c35d0ce904a8b899
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-01-27 23:52:03 -0600

    save the las change of chapter 10

--- Chapter 10/remember_me.py
+++ Chapter 10/remember_me.py
@@ -1,8 +1,18 @@
 import json
 
-username = input("What is your name? ")
+# Load the username, if it has been stored previouslu,
+# Otherwise, prompt for the username and store it
+
 
 filename = 'username.json'
-with open(filename, 'w') as f_obj:
-    json.dump(username, f_obj)
-    print("We'll remember you when you come back, " + username + "!" )
+try:
+    with open(filename) as f_obj:
+        username = json.load(f_obj)
+
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+        print("We'll remember you when you come back, " + username + "!" )
+else:
+    print("Welcome back, " + username + "!")
